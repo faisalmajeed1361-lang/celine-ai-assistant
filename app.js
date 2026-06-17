7aed1e65b2950b4d78ec337e698d9ba917358d87
faisalmajeed1361-lang <faisalmajeed1361@gmail.com>
2026-06-16 22:09:37 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const aiRoutes = require('./routes/aiRoutes');
+
+app.use(express.json());
+
+// Main Routes
+app.use('/api/ai', aiRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Celine AI Assistant Server is Running!');
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server is live on port ${PORT}`);
+});
